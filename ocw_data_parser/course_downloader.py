c8dc99b7a996858e1e901ac7c0d5e7cd8dcd7c59
Carey Gumaer <gumaer.carey@gmail.com>
2020-06-30 11:59:50 -0400

    add some commenting on OCWDownloader and an example courses.json

--- ocw_data_parser/course_downloader.py
+++ ocw_data_parser/course_downloader.py
@@ -3,7 +3,11 @@ import shutil
 import json
 import boto3
 
+"""
+This is a class used for downloading source json from S3 based on a list of course id's
 
+An example of the expected format can be found in example_courses.json
+"""
 class OCWDownloader(object):
     def __init__(self,
                  courses_json="",
@@ -29,19 +33,25 @@ class OCWDownloader(object):
         allObjects = ocwContentStorage.meta.client.list_objects(
             Bucket=ocwContentStorage.name, Prefix="{}/".format(self.env), Delimiter="/")
 
+        # first tier subfolders under env should be department numbers
         for departmentPath in allObjects.get("CommonPrefixes"):
+            # second tier subfolders should be course numbers
             courseNumbers = ocwContentStorage.meta.client.list_objects(
                 Bucket=ocwContentStorage.name, Prefix=departmentPath.get("Prefix"), Delimiter="/")
             for courseNumberPath in courseNumbers.get("CommonPrefixes"):
+                # third tier folders should be course terms
                 courseTerms = ocwContentStorage.meta.client.list_objects(
                     Bucket=ocwContentStorage.name, Prefix=courseNumberPath.get("Prefix"), Delimiter="/")
                 for courseTerm in courseTerms.get("CommonPrefixes"):
+                    # fourth tier folders should be course folders keyed with the course id
                     courseFolders = ocwContentStorage.meta.client.list_objects(
                         Bucket=ocwContentStorage.name, Prefix=courseTerm.get("Prefix"), Delimiter="/")
                     for courseFolder in courseFolders.get("CommonPrefixes"):
+                        # make sure the course is in our list
                         courseId = os.path.basename(
                             os.path.normpath(courseFolder.get("Prefix")))
                         if courseId in courses:
+                            # make the destination path if it doesn't exist and download all files
                             rawCoursePath = os.path.join(
                                 self.destinationDir, courseId, "0")
                             if not os.path.exists(rawCoursePath):
