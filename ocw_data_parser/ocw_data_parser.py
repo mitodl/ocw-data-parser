17d50f578022d3201d4fa3378258badd03e618a2
Carey P Gumaer <gumaerc@mit.edu>
2020-10-13 15:21:24 -0400

    publishing dates (#51)
    * add a pipfile for this project since it doesn't have one
    
    * add last _published_to_production and last_unpublishing_date
    
    * add retirement_date
    
    * remove pipfiles as they don't belong in this PR
    
    * not sure how this made it in, but it doesn't belong here

--- ocw_data_parser/ocw_data_parser.py
+++ ocw_data_parser/ocw_data_parser.py
@@ -133,6 +133,9 @@ class OCWParser(object):
         new_json["title"] = safe_get(self.jsons[0], "title")
         new_json["description"] = safe_get(self.jsons[1], "description")
         new_json["other_information_text"] = safe_get(self.jsons[1], "other_information_text")
+        new_json["last_published_to_production"] = safe_get(self.jsons[0], "last_published_to_production")
+        new_json["last_unpublishing_date"] = safe_get(self.jsons[0], "last_unpublishing_date")
+        new_json["retirement_date"] = safe_get(self.jsons[0], "retirement_date")
         new_json["sort_as"] = safe_get(self.jsons[0], "sort_as")
         master_course = safe_get(self.jsons[0], "master_course_number")
         if master_course:
